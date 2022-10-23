aeb2692c3b4e1a703019ce76ed96c4ad0c0476da
corner <corner3499@gmail.com>
2022-10-23 23:11:12 +0900

    routing

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
 Nuxt에서는 라우터 이동할때 NuxtLink 태그를 사용하며, 경로는 pages폴더 안에 있는 `폴더명/파일명`이 경로가 된다.
 ex) apple폴더안에 main.vue  localhost:8080/apple/apple
 -->
+
     <NuxtLink to="/apple/main">
       Move to "ApplePage"
     </NuxtLink>
@@ -14,6 +15,5 @@ ex) apple폴더안에 main.vue  localhost:8080/apple/apple
 <script>
 export default {
   name: 'IndexPage',
-  layout: 'AppleLayout',
 }
 </script>
